7aff51a908c795011949884e0f4caa254a2c504f
Naoki Kimura <n.kimura.cap@gmail.com>
2019-06-28 16:00:14 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,6 @@ const findLocalByPath = (source: string): Promise<any> => readdir(source, { with
       return entry.dirent.isDirectory() ? findLocalByPath(entry.path) : entry;
     }));
   });
-
 const findRemoteById = (id: string, folderPath: string = '') => client.folders.getItems(id).then((items: any) => {
   return Promise.all(items.entries.map((entry: any) => {
     const entryPath = path.join(folderPath, entry.name);
@@ -57,6 +56,23 @@ const findRemoteFolderByPath = (folderPath: string[], rootFolder: any) => {
   });
 };
 
+const createRemoteFolderByPath = (folderPath: string[], rootFolder: any) => {
+  debug(`${folderPath},${rootFolder.id}`);
+  if (folderPath.length === 0 || rootFolder === undefined) {
+    return rootFolder === undefined || rootFolder.item_collection ? Promise.resolve(rootFolder) : client.folders.get(rootFolder.id);
+  }
+
+  const folderName = _.first(folderPath);
+  return client.folders.getItems(rootFolder.id).then((items: any) => {
+    const entries: Array<any> = items.entries;
+    const subFolder = _.first(entries.filter(entry => entry.type === 'folder' && entry.name === folderName));
+    return new Promise((resolve, reject) => {
+      debug(`${subFolder},${rootFolder.id},${folderName}`);
+      return subFolder ? resolve(subFolder) : client.folders.create(rootFolder.id, folderName).then(resolve);
+    }).then((folder: any) => createRemoteFolderByPath(folderPath.slice(1), folder));
+  });
+};
+
 const digest = (source: string) => new Promise<string>((resolve, reject) => {
   const hash = crypto.createHash('sha1');
   const stream = fs.createReadStream(source);
@@ -70,7 +86,7 @@ client.folders.get(destination).then((rootFolder: any) => {
   Promise.all([
     findLocalByPath(source).then(_.flattenDeep)
     .then(entries => Promise.all(entries.map((entry: any) => digest(entry.path).then(sha1 => {
-      return Object.assign({sha1}, entry);
+      return _.assign({sha1}, entry);
     })))),
     findRemoteById(rootFolder.id).then(_.flattenDeep),
   ])
@@ -80,12 +96,28 @@ client.folders.get(destination).then((rootFolder: any) => {
     return o;
   }, {})))
   .then(([local, remote]) => _.merge(local, remote))
-  .then(map => _.keys(map).forEach(key => {
-    const entry = map[key];
-    debug(entry);
-    if (entry.sha1 === entry && entry.entry.sha1) {
-      debug(`${key} is synchronized.`);
-      return;
+  .then(map => _.values(map).forEach(e => {
+    debug(e);
+    if (!e.dirent) {
+      debug(`'${e.path}' only exists remotely.`);
+    } else if (e.entry) {
+      if (e.sha1 === e.entry.sha1) {
+        debug(`'${e.path}' is synchronized.`);
+      } else {
+        client.files.uploadNewFileVersion(e.entry.id, fs.createReadStream(e.path))
+          .then((file: any) => {
+            debug(`A new version of '${e.path}' has been uploaded.`);
+          });
+      }
+    } else {
+      const { dir, base } = path.parse(e.path);
+      const dirs = dir === '' ? [] : dir.split(path.sep);
+      findRemoteFolderByPath(dirs, rootFolder)
+        .then((folder: any) => folder || createRemoteFolderByPath(dirs, rootFolder))
+        .then((folder: any) => client.files.uploadFile(folder.id, base, fs.createReadStream(e.path)))
+        .then((file: any) => {
+          debug(`'${e.path}' is newly uploaded.`);
+        })
     }
   }))
 });
