186d6179f8f51aa4ac1f75b9d07255c97b9d7212
Naoki Kimura <n.kimura.cap@gmail.com>
2019-07-07 23:04:02 +0900

    Refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import BoxSDK from 'box-node-sdk';
-import _ from 'lodash';
 import crypto from 'crypto';
 import fs from 'fs';
+import _ from 'lodash';
 import path from 'path';
 import util from 'util';
 
@@ -17,34 +17,12 @@ const INIT_RETRY_TIMES = 5;
 const debug = util.debuglog('carp-streamer:app');
 
 export abstract class Entry {
-  constructor(protected root: string, readonly relativePath: string, protected dirent: fs.Dirent | undefined, protected remoteRoot: BoxSDK.Folder) {
-  }
 
   get absolutePath() {
     return path.resolve(this.root, this.relativePath);
   }
 
-  synchronize(client: BoxSDK.BoxClient, pretend: boolean = false) {
-    return this._synchronize(client, pretend, INIT_RETRY_TIMES, 0);
-  }
-
-  private async _synchronize(client: BoxSDK.BoxClient, pretend: boolean, retryTimes: number, delay: number): Promise<ResultStatus> {
-    await sleep(delay);
-    try {
-      return await this.sync(client, pretend);
-    } catch (error) {
-      if (!isBoxAPIResponseError(error)) throw error;
-      debug('API Response Error: %s', error.message);
-      if (!(error.statusCode === 429 && retryTimes > 0)) throw error;
-
-      debug('Retries %d more times.', retryTimes);
-      const retryAfter = (Number(error.response.headers['retry-after'] || 0) + Math.floor(Math.random() * 10 * (1 / retryTimes))) * 1000;
-      debug('Tries again in %d milliseconds.', retryAfter);
-      return this._synchronize(client, pretend, retryTimes - 1, retryAfter);
-    }
-  }
-
-  static create(dirent: fs.Dirent | null, root: string, relativePath: string, remoteRoot: BoxSDK.Folder, client: BoxSDK.BoxClient): Promise<Entry> {
+  public static create(dirent: fs.Dirent | null, root: string, relativePath: string, remoteRoot: BoxSDK.Folder, client: BoxSDK.BoxClient): Promise<Entry> {
     return Entry._create(dirent, root, relativePath, remoteRoot, client, INIT_RETRY_TIMES, 0);
   }
 
@@ -55,37 +33,66 @@ export abstract class Entry {
         // TODO:
         throw new Error('Not Implemented Error');
       } else if (dirent.isDirectory()) {
-        return new Directory(root, relativePath, dirent, remoteRoot, await findRemoteFolderByPath(relativePath, remoteRoot, client));
+        const remoteFolder = await findRemoteFolderByPath(relativePath, remoteRoot, client);
+        return new Directory(root, relativePath, dirent, remoteRoot, remoteFolder);
       } else {
-        return new File(root, relativePath, dirent, remoteRoot, await findRemoteFileByPath(relativePath, remoteRoot, client));
+        const remoteFile = await findRemoteFileByPath(relativePath, remoteRoot, client);
+        return new File(root, relativePath, dirent, remoteRoot, remoteFile);
       }
     } catch (error) {
-      if (!isBoxAPIResponseError(error)) throw error;
+      if (!isBoxAPIResponseError(error)) { throw error; }
       debug('API Response Error: %s', error.message);
-      if (!(error.statusCode === 429 && retryTimes > 0)) throw error;
+      if (!(error.statusCode === 429 && retryTimes > 0)) { throw error; }
 
       debug('Retries %d more times.', retryTimes);
-      const retryAfter = (Number(error.response.headers['retry-after'] || 0) + Math.floor(Math.random() * 10 * (1 / retryTimes))) * 1000;
+      const retryAfter = determineDelayTime(error, retryTimes);
       debug('Tries again in %d milliseconds.', retryAfter);
       return this._create(dirent, root, relativePath, remoteRoot, client, retryTimes - 1, retryAfter);
     }
   }
 
+  constructor(protected root: string, readonly relativePath: string, protected dirent: fs.Dirent | undefined, protected remoteRoot: BoxSDK.Folder) { }
+
+  public synchronize(client: BoxSDK.BoxClient, pretend: boolean = false) {
+    return this._synchronize(client, pretend, INIT_RETRY_TIMES, 0);
+  }
+
   protected abstract sync(client: BoxSDK.BoxClient, pretend: boolean): Promise<ResultStatus>;
+
+  private async _synchronize(client: BoxSDK.BoxClient, pretend: boolean, retryTimes: number, delay: number): Promise<ResultStatus> {
+    await sleep(delay);
+    try {
+      return await this.sync(client, pretend);
+    } catch (error) {
+      if (!isBoxAPIResponseError(error)) { throw error; }
+      debug('API Response Error: %s', error.message);
+      if (!(error.statusCode === 429 && retryTimes > 0)) { throw error; }
+
+      debug('Retries %d more times.', retryTimes);
+      const retryAfter = determineDelayTime(error, retryTimes);
+      debug('Tries again in %d milliseconds.', retryAfter);
+      return this._synchronize(client, pretend, retryTimes - 1, retryAfter);
+    }
+  }
 }
 
-export class Directory extends Entry {
-  constructor(root: string, relativePath: string, dirent: fs.Dirent | undefined, remoteRoot: BoxSDK.Folder, private remoteFile: BoxSDK.MiniFolder | undefined) {
+function determineDelayTime(error: BoxAPIResponseError, retryTimes: number): number {
+  const retryAfter = Number(error.response.headers['retry-after'] || 0);
+  return (retryAfter + Math.floor(Math.random() * 10 * (1 / retryTimes))) * 1000;
+}
+
+class Directory extends Entry {
+  constructor(root: string, relativePath: string, dirent: fs.Dirent | undefined, remoteRoot: BoxSDK.Folder, private remoteFolder: BoxSDK.MiniFolder | undefined) {
     super(root, relativePath, dirent, remoteRoot);
   }
 
   protected async sync(client: BoxSDK.BoxClient, pretend: boolean = false): Promise<ResultStatus> {
-    if (!pretend) await createRemoteFolderUnlessItExists(this.relativePath, this.remoteRoot, client);
+    if (!pretend) { await createRemoteFolderUnlessItExists(this.relativePath, this.remoteRoot, client); }
     return ResultStatus.SYNCHRONIZED;
   }
 }
 
-export class File extends Entry {
+class File extends Entry {
   constructor(root: string, relativePath: string, dirent: fs.Dirent | undefined, remoteRoot: BoxSDK.Folder, private remoteFile: BoxSDK.MiniFile | undefined) {
     super(root, relativePath, dirent, remoteRoot);
   }
@@ -99,7 +106,7 @@ export class File extends Entry {
         const { dir, base } = path.parse(this.relativePath);
         const folder = await createRemoteFolderUnlessItExists(dir, this.remoteRoot, client);
         debug('Uploading `%s`...', this.relativePath);
-        await client.files.uploadFile(folder.id, base, this.createReadStream());  
+        await client.files.uploadFile(folder.id, base, this.createReadStream());
       }
       return ResultStatus.UPLOADED;
     } else {
@@ -127,7 +134,7 @@ export class File extends Entry {
   }
 
   private createReadStream() {
-    return fs.createReadStream(this.absolutePath)
+    return fs.createReadStream(this.absolutePath);
   }
 }
 
@@ -136,7 +143,8 @@ interface BoxAPIResponseError extends Error {
   response: any;
   request: any;
 }
-const isBoxAPIResponseError = (error: any): error is BoxAPIResponseError => error.statusCode && error.response && error.request && error instanceof Error;
+const isBoxAPIResponseError = (error: any): error is BoxAPIResponseError =>
+  error.statusCode && error.response && error.request && error instanceof Error;
 
 const isMiniFile = (item: BoxSDK.Item): item is BoxSDK.MiniFile => item.type === 'file';
 
@@ -168,7 +176,7 @@ async function _findRemoteFolderByPath(folderPath: string[], rootFolder: BoxSDK.
   const folderName = _.first(folderPath) || '';
   const subFolder = await findRemoteFolderByName(folderName, client, rootFolder.id);
   return _findRemoteFolderByPath(folderPath.slice(1), subFolder, client);
-};
+}
 
 const createRemoteFolderByPath = async (folderPath: string[], rootFolder: BoxSDK.MiniFolder, client: BoxSDK.BoxClient): Promise<BoxSDK.Folder> => {
   if (folderPath.length === 0) {
@@ -188,18 +196,20 @@ function sleep(delay: number): Promise<void> {
 async function* fetchRemoteFolderItems(client: BoxSDK.BoxClient, parentFolderId: string, marker?: string): AsyncIterableIterator<BoxSDK.Item> {
   const items = await client.folders.getItems(parentFolderId, { usemarker: true, marker });
   yield* items.entries;
-  if (items.next_marker) yield* fetchRemoteFolderItems(client, parentFolderId, items.next_marker);
+  if (items.next_marker) { yield* fetchRemoteFolderItems(client, parentFolderId, items.next_marker); }
 }
 
 async function findRemoteFolderByName(folderName: string, client: BoxSDK.BoxClient, parentFolderId: string): Promise<BoxSDK.MiniFolder | undefined> {
-  for await (let item of fetchRemoteFolderItems(client, parentFolderId)) {
-    if (isMiniFolder(item) && item.name == folderName) return item;
-  }
+  return findRemoteItemByName<BoxSDK.MiniFolder>(folderName, parentFolderId, isMiniFolder, client);
 }
 
 async function findRemoteFileByName(fileName: string, client: BoxSDK.BoxClient, parentFolderId: string): Promise<BoxSDK.MiniFile | undefined> {
-  for await (let item of fetchRemoteFolderItems(client, parentFolderId)) {
-    if (isMiniFile(item) && item.name.normalize() == fileName.normalize()) return item;
+  return findRemoteItemByName<BoxSDK.MiniFile>(fileName, parentFolderId, isMiniFile, client);
+}
+
+async function findRemoteItemByName<T extends BoxSDK.Item>(itemName: string, parentFolderId: string, isItem: (item: BoxSDK.Item) => item is T, client: BoxSDK.BoxClient): Promise<T | undefined> {
+  for await (const item of fetchRemoteFolderItems(client, parentFolderId)) {
+    if (isItem(item) && item.name.normalize() === itemName.normalize()) { return item; }
   }
 }
 
@@ -209,9 +219,9 @@ async function createRemoteFolder(client: BoxSDK.BoxClient, parentFolderId: stri
     return await client.folders.create(parentFolderId, folderName);
   } catch (error) {
     debug(`Failed to create folder '%s' (parent folder id: %s).`, folderName, parentFolderId);
-    if (!isBoxAPIResponseError(error)) throw error;
+    if (!isBoxAPIResponseError(error)) { throw error; }
     debug('API Response Error: %s', error.message);
-    if (!(error.statusCode === 409 && retryTimes > 0)) throw error;
+    if (!(error.statusCode === 409 && retryTimes > 0)) { throw error; }
 
     const folder = await findRemoteFolderByName(folderName, client, parentFolderId);
     if (folder) {
@@ -227,27 +237,28 @@ async function createRemoteFolder(client: BoxSDK.BoxClient, parentFolderId: stri
 async function createRemoteFolderUnlessItExists(relativePath: string, rootFolder: BoxSDK.MiniFolder, client: BoxSDK.BoxClient): Promise<BoxSDK.Folder> {
   const dirs = !relativePath ? [] : relativePath.split(path.sep);
   const foundFolder = await _findRemoteFolderByPath(dirs, rootFolder, client);
-  return foundFolder || await createRemoteFolderByPath(dirs, rootFolder, client)
+  return foundFolder || await createRemoteFolderByPath(dirs, rootFolder, client);
 }
 
 export function createDirentFromStats(stats: fs.Stats, name: string): fs.Dirent {
+  // tslint:disable-next-line: no-empty-interface
   interface DirentLike extends fs.Dirent { }
   return new class implements DirentLike {
     get name() { return name; }
-    isBlockDevice() { return stats.isBlockDevice(); }
-    isCharacterDevice() { return stats.isCharacterDevice(); }
-    isDirectory() { return stats.isDirectory(); }
-    isFIFO() { return stats.isFIFO(); }
-    isFile() { return stats.isFile(); }
-    isSocket() { return stats.isSocket(); }
-    isSymbolicLink() { return stats.isSymbolicLink(); }
-  };
+    public isBlockDevice() { return stats.isBlockDevice(); }
+    public isCharacterDevice() { return stats.isCharacterDevice(); }
+    public isDirectory() { return stats.isDirectory(); }
+    public isFIFO() { return stats.isFIFO(); }
+    public isFile() { return stats.isFile(); }
+    public isSocket() { return stats.isSocket(); }
+    public isSymbolicLink() { return stats.isSymbolicLink(); }
+  }();
 }
 
 const readdirAsync = util.promisify(fs.readdir);
-export async function* listDirectoryEntriesRecursively(root: string): AsyncIterableIterator<{path: string, dirent: fs.Dirent | null, error:any}> {
+export async function* listDirectoryEntriesRecursively(root: string): AsyncIterableIterator<{ path: string, dirent: fs.Dirent | null, error: any }> {
   try {
-    for(let dirent of await readdirAsync(root, { withFileTypes: true })) {
+    for (const dirent of await readdirAsync(root, { withFileTypes: true })) {
       const entryPath = path.join(root, dirent.name);
       yield ({ path: entryPath, dirent, error: null });
       if (dirent.isDirectory()) {
@@ -255,6 +266,6 @@ export async function* listDirectoryEntriesRecursively(root: string): AsyncItera
       }
     }
   } catch (error) {
-    yield { path: root, dirent: null, error};
+    yield { path: root, dirent: null, error };
   }
 }
