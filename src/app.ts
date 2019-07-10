c1df872798f855d4e39618aa4b2ec1718cc7cf2a
Naoki Kimura <n.kimura.cap@gmail.com>
2019-07-10 10:20:21 +0900

    extract class BoxFinder

--- src/app.ts
+++ src/app.ts
@@ -28,16 +28,17 @@ export abstract class Entry {
   }
 
   private static async _create(dirent: fs.Dirent | null, root: string, relativePath: string, remoteRoot: BoxSDK.Folder, client: BoxSDK.BoxClient, retryTimes: number, delay: number): Promise<Entry> {
+    const finder = new BoxFinder(client, remoteRoot);
     await sleep(delay);
     try {
       if (!dirent) {
         // TODO:
         throw new Error('Not Implemented Error');
       } else if (dirent.isDirectory()) {
-        const remoteFolder = await findRemoteFolderByPath(relativePath, remoteRoot, client);
+        const remoteFolder = await finder.findFolderByPath(relativePath);
         return new Directory(root, relativePath, dirent, remoteRoot, remoteFolder);
       } else {
-        const remoteFile = await findRemoteFileByPath(relativePath, remoteRoot, client);
+        const remoteFile = await finder.findFileByPath(relativePath);
         return new File(root, relativePath, dirent, remoteRoot, remoteFile);
       }
     } catch (error) {
@@ -91,7 +92,7 @@ class Directory extends Entry {
     if (this.remoteFolder) {
       return ResultStatus.SYNCHRONIZED;
     } else {
-      if (!pretend) { await createRemoteFolderUnlessItExists(this.relativePath, this.remoteRoot, client); }
+      if (!pretend) { await new BoxFinder(client, this.remoteRoot).createFolderUnlessItExists(this.relativePath); }
       return ResultStatus.CREATED;
     }
   }
@@ -109,7 +110,7 @@ class File extends Entry {
     } else if (!this.remoteFile) {
       if (!pretend) {
         const { dir, base } = path.parse(this.relativePath);
-        const folder = await createRemoteFolderUnlessItExists(dir, this.remoteRoot, client);
+        const folder = await new BoxFinder(client, this.remoteRoot).createFolderUnlessItExists(dir);
         debug('Uploading `%s`...', this.relativePath);
         await client.files.uploadFile(folder.id, base, this.createReadStream());
       }
@@ -148,101 +149,115 @@ interface BoxAPIResponseError extends Error {
   response: any;
   request: any;
 }
+
 const isBoxAPIResponseError = (error: any): error is BoxAPIResponseError =>
   error.statusCode && error.response && error.request && error instanceof Error;
 
 const isMiniFile = (item: BoxSDK.Item): item is BoxSDK.MiniFile => item.type === 'file';
 
-function findRemoteFileByPath(relativePath: string, rootFolder: BoxSDK.MiniFolder, client: BoxSDK.BoxClient): Promise<BoxSDK.File | undefined> {
-  const { dir, base } = path.parse(relativePath);
-  const dirs = dir === '' ? [] : dir.split(path.sep);
-  return _findRemoteFileByPath(dirs, base, rootFolder, client);
-}
-
-async function _findRemoteFileByPath(folderPath: string[], filename: string, rootFolder: BoxSDK.MiniFolder, client: BoxSDK.BoxClient): Promise<BoxSDK.MiniFile | undefined> {
-  const folder = await _findRemoteFolderByPath(folderPath, rootFolder, client);
-  return !folder ? folder : await findRemoteFileByName(filename, client, folder.id);
-}
-
 const isFolder = (item: BoxSDK.MiniFolder): item is BoxSDK.Folder => (item as BoxSDK.Folder).size !== undefined;
 const isMiniFolder = (item: BoxSDK.Item): item is BoxSDK.MiniFolder => item.type === 'folder';
-
-function findRemoteFolderByPath(relativePath: string, rootFolder: BoxSDK.MiniFolder | undefined, client: BoxSDK.BoxClient): Promise<BoxSDK.Folder | undefined> {
-  const { dir, base } = path.parse(relativePath);
-  const dirs = (dir === '' ? [] : dir.split(path.sep)).concat(base);
-  return _findRemoteFolderByPath(dirs, rootFolder, client);
-}
-
-async function _findRemoteFolderByPath(folderPath: string[], rootFolder: BoxSDK.MiniFolder | undefined, client: BoxSDK.BoxClient): Promise<BoxSDK.Folder | undefined> {
-  if (folderPath.length === 0 || rootFolder === undefined) {
-    return rootFolder === undefined || isFolder(rootFolder) ? rootFolder : await client.folders.get(rootFolder.id);
-  }
-
-  const folderName = _.first(folderPath) || '';
-  const subFolder = await findRemoteFolderByName(folderName, client, rootFolder.id);
-  return _findRemoteFolderByPath(folderPath.slice(1), subFolder, client);
-}
-
-const createRemoteFolderByPath = async (folderPath: string[], rootFolder: BoxSDK.MiniFolder, client: BoxSDK.BoxClient): Promise<BoxSDK.Folder> => {
-  if (folderPath.length === 0) {
-    return isFolder(rootFolder) ? rootFolder : await client.folders.get(rootFolder.id);
-  }
-
-  const folderName = _.first(folderPath) || '';
-  const subFolder = await findRemoteFolderByName(folderName, client, rootFolder.id);
-  const folder = subFolder || await createRemoteFolder(client, rootFolder.id, folderName, INIT_RETRY_TIMES, 0);
-  return createRemoteFolderByPath(folderPath.slice(1), folder, client);
-};
 
 function sleep(delay: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
 
-async function* fetchRemoteFolderItems(client: BoxSDK.BoxClient, parentFolderId: string, marker?: string): AsyncIterableIterator<BoxSDK.Item> {
-  const items = await client.folders.getItems(parentFolderId, { usemarker: true, marker });
-  yield* items.entries;
-  if (items.next_marker) { yield* fetchRemoteFolderItems(client, parentFolderId, items.next_marker); }
-}
+class BoxFinder {
 
-async function findRemoteFolderByName(folderName: string, client: BoxSDK.BoxClient, parentFolderId: string): Promise<BoxSDK.MiniFolder | undefined> {
-  return findRemoteItemByName<BoxSDK.MiniFolder>(folderName, parentFolderId, isMiniFolder, client);
-}
-
-async function findRemoteFileByName(fileName: string, client: BoxSDK.BoxClient, parentFolderId: string): Promise<BoxSDK.MiniFile | undefined> {
-  return findRemoteItemByName<BoxSDK.MiniFile>(fileName, parentFolderId, isMiniFile, client);
-}
-
-async function findRemoteItemByName<T extends BoxSDK.Item>(itemName: string, parentFolderId: string, isItem: (item: BoxSDK.Item) => item is T, client: BoxSDK.BoxClient): Promise<T | undefined> {
-  for await (const item of fetchRemoteFolderItems(client, parentFolderId)) {
-    if (isItem(item) && item.name.normalize() === itemName.normalize()) { return item; }
+  public static async create(client: BoxSDK.BoxClient, folderId = '0') {
+    const folder = await client.folders.get(folderId);
+    return new BoxFinder(client, folder);
   }
-}
 
-async function createRemoteFolder(client: BoxSDK.BoxClient, parentFolderId: string, folderName: string, retryTimes: number, delay: number): Promise<BoxSDK.Folder> {
-  await sleep(delay);
-  try {
-    return await client.folders.create(parentFolderId, folderName);
-  } catch (error) {
-    debug(`Failed to create folder '%s' (parent folder id: %s).`, folderName, parentFolderId);
-    if (!isBoxAPIResponseError(error)) { throw error; }
-    debug('API Response Error: %s', error.message);
-    if (!(error.statusCode === 409 && retryTimes > 0)) { throw error; }
+  private static async createFolderByPath(folderPath: string[], finder: BoxFinder): Promise<BoxSDK.Folder> {
+    if (folderPath.length === 0) {
+      return isFolder(finder.current) ? finder.current : await finder.client.folders.get(finder.current.id);
+    }
+    const folderName = _.first(folderPath) || '';
+    const subFolder = await finder.findFolderByName(folderName);
+    const folder = subFolder || await finder.createFolder(folderName);
+    return this.createFolderByPath(folderPath.slice(1), finder.new(folder));
+  }
 
-    const folder = await findRemoteFolderByName(folderName, client, parentFolderId);
-    if (folder) {
-      return client.folders.get(folder.id);
-    } else {
-      const retryAfter = (Math.floor(Math.random() * 100) * (1 / retryTimes)) * 1000;
-      debug(`Waiting time is %d milliseconds.`, retryAfter);
-      return createRemoteFolder(client, parentFolderId, folderName, retryTimes - 1, retryAfter);
+  private static async _findFolderByPath(folderPath: string[], finder?: BoxFinder): Promise<BoxSDK.Folder | undefined> {
+    if (finder === undefined) { return undefined; }
+    if (folderPath.length === 0) {
+      return isFolder(finder.current) ? finder.current : await finder.client.folders.get(finder.current.id);
+    }
+
+    const folderName = _.first(folderPath) || '';
+    const subFolder = await finder.findFolderByName(folderName);
+    return BoxFinder._findFolderByPath(folderPath.slice(1), subFolder && finder.new(subFolder));
+  }
+
+  constructor(private client: BoxSDK.BoxClient, private current: BoxSDK.MiniFolder) {
+  }
+
+  public async createFolderUnlessItExists(relativePath: string): Promise<BoxSDK.Folder> {
+    const dirs = !relativePath ? [] : relativePath.split(path.sep);
+    const foundFolder = await BoxFinder._findFolderByPath(dirs, this);
+    return foundFolder || await BoxFinder.createFolderByPath(dirs, this);
+  }
+
+  public async findFileByPath(relativePath: string): Promise<BoxSDK.File | undefined> {
+    const { dir, base } = path.parse(relativePath);
+    const dirs = dir === '' ? [] : dir.split(path.sep);
+    const folder = await BoxFinder._findFolderByPath(dirs, this);
+    return folder && await this.new(folder).findFileByName(base);
+  }
+
+  public findFolderByPath(relativePath: string): Promise<BoxSDK.Folder | undefined> {
+    const { dir, base } = path.parse(relativePath);
+    const dirs = (dir === '' ? [] : dir.split(path.sep)).concat(base);
+    return BoxFinder._findFolderByPath(dirs, this);
+  }
+
+  private new(folder: BoxSDK.MiniFolder) {
+    return new BoxFinder(this.client, folder);
+  }
+
+  private async createFolder(folderName: string, retryTimes = INIT_RETRY_TIMES, delay = 0): Promise<BoxSDK.Folder> {
+    const parentFolderId = this.current.id;
+    await sleep(delay);
+    try {
+      return await this.client.folders.create(parentFolderId, folderName);
+    } catch (error) {
+      debug(`Failed to create folder '%s' (parent folder id: %s).`, folderName, parentFolderId);
+      if (!isBoxAPIResponseError(error)) { throw error; }
+      debug('API Response Error: %s', error.message);
+      if (!(error.statusCode === 409 && retryTimes > 0)) { throw error; }
+
+      const folder = await this.findFolderByName(folderName);
+      if (folder) {
+        return this.client.folders.get(folder.id);
+      } else {
+        const retryAfter = (Math.floor(Math.random() * 100) * (1 / retryTimes)) * 1000;
+        debug(`Waiting time is %d milliseconds.`, retryAfter);
+        return this.createFolder(folderName, retryTimes - 1, retryAfter);
+      }
     }
   }
-}
 
-async function createRemoteFolderUnlessItExists(relativePath: string, rootFolder: BoxSDK.MiniFolder, client: BoxSDK.BoxClient): Promise<BoxSDK.Folder> {
-  const dirs = !relativePath ? [] : relativePath.split(path.sep);
-  const foundFolder = await _findRemoteFolderByPath(dirs, rootFolder, client);
-  return foundFolder || await createRemoteFolderByPath(dirs, rootFolder, client);
+  private findFileByName(fileName: string): Promise<BoxSDK.MiniFile | undefined> {
+    return this.findItemByName<BoxSDK.MiniFile>(fileName, isMiniFile);
+  }
+
+  private findFolderByName(folderName: string): Promise<BoxSDK.MiniFolder | undefined> {
+    return this.findItemByName<BoxSDK.MiniFolder>(folderName, isMiniFolder);
+  }
+
+  private async findItemByName<T extends BoxSDK.Item>(itemName: string, isItem: (item: BoxSDK.Item) => item is T): Promise<T | undefined> {
+    for await (const item of this.fetchFolderItems()) {
+      if (isItem(item) && item.name.normalize() === itemName.normalize()) { return item; }
+    }
+  }
+
+  private async * fetchFolderItems(marker?: string): AsyncIterableIterator<BoxSDK.Item> {
+    const parentFolderId = this.current.id;
+    const items = await this.client.folders.getItems(parentFolderId, { usemarker: true, marker });
+    yield* items.entries;
+    if (items.next_marker) { yield* this.fetchFolderItems(items.next_marker); }
+  }
 }
 
 export function createDirentFromStats(stats: fs.Stats, name: string): fs.Dirent {
