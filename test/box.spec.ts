d2bdb981624906c12e6ae910d0ed805d71864b42
Naoki Kimura <n.kimura.cap@gmail.com>
2019-07-27 22:26:32 +0900

    Create box.spec.ts

---
+++ test/box.spec.ts
@@ -0,0 +1,46 @@
+const AnonymousSession = require('box-node-sdk/lib/sessions/anonymous-session');
+const AppAuthSession = require('box-node-sdk/lib/sessions/app-auth-session');
+const BasicSession = require('box-node-sdk/lib/sessions/basic-session');
+const PersistentSession = require('box-node-sdk/lib/sessions/persistent-session');
+import { expect } from 'chai';
+import { BoxAppConfig, BoxClientBuilder, BoxClientConfig } from '../src/box';
+
+describe('box', () => {
+  describe('BoxClientBuilder', () => {
+    it('anonymous client', done => {
+      const client = new BoxClientBuilder().build();
+      expect(client).to.have.property('_session').to.be.an.instanceOf(AnonymousSession);
+      done();
+    });
+
+    it('app auth client', done => {
+      const appConfig: BoxAppConfig = { boxAppSettings: { clientID: '', clientSecret: '' }, enterpriseID: 'foo' };
+      const clientConfig: BoxClientConfig = { kind: 'AppAuth', type: 'enterprise' };
+      const client = new BoxClientBuilder(appConfig, clientConfig).build();
+      expect(client).to.have.property('_session').to.be.an.instanceOf(AppAuthSession);
+      done();
+    });
+
+    it('basic client', done => {
+      const appConfig: BoxAppConfig = { boxAppSettings: { clientID: '', clientSecret: '' } };
+      const clientConfig: BoxClientConfig = { kind: 'Basic', accessToken: '' };
+      const client = new BoxClientBuilder(appConfig, clientConfig).build();
+      expect(client).to.have.property('_session').to.be.an.instanceOf(BasicSession);
+      done();
+    });
+
+    it('persistent client', done => {
+      const appConfig: BoxAppConfig = { boxAppSettings: { clientID: '', clientSecret: '' } };
+      const tokenInfo = {
+        accessToken: 'foo',
+        accessTokenTTLMS: -1,
+        acquiredAtMS: -1,
+        refreshToken: 'bar',
+      };
+      const clientConfig: BoxClientConfig = { kind: 'Persistent', tokenInfo };
+      const client = new BoxClientBuilder(appConfig, clientConfig).build();
+      expect(client).to.have.property('_session').to.be.an.instanceOf(PersistentSession);
+      done();
+    });
+  });
+});
